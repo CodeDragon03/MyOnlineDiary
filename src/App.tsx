4c18ff8fd710a029e3a070adde7d31a27e9477db
CodeDragon03 <jay7666.professional@gmail.com>
2025-08-20 16:26:01 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/** TODO:
+ *
+ * Add the routes
+ */
+
+const App = () => {
+    return (
+        <>
+            {/* TODO: Your Code Here */}
+            <div></div>
+        </>
+    );
+};
+
+export default App;
