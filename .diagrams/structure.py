661efd5296c2cc190fb849098ef479f8b16cd75b
CodeDragon03 <jay7666.professional@gmail.com>
2025-10-19 12:10:43 +0530

    Adding cypress test setup.

---
+++ .diagrams/structure.py
@@ -0,0 +1,10 @@
+from diagrams import Diagram
+
+
+def main():
+    with Diagram("Folder Structure", show=False):
+        pass  # Add diagram components here
+
+
+if __name__ == "__main__":
+    main()
